6eaedba6ebbcd854457b405d7c072d41e9814540
Heli Hyvättinen <heli.hyvattinen@netti.fi>
2018-10-03 10:00:13 +0300

    Tehtävä 5.14 valmistelua
    Uudelleennimetty BlogItem->Blog ja siirretty se omaan tiedostoonsa.

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -3,6 +3,7 @@ import blogService from './services/blogs'
 import loginService from './services/login'
 import './App.css'
 import PropTypes from 'prop-types'
+import Blog from './components/Blog'
 
 const Notification = ({message, isError}) => {
   if (message === null) {
@@ -99,116 +100,7 @@ BlogForm.propTypes = {
   url: PropTypes.string.isRequired
 }
 
-const DeleteButton = ({onClick,blog,user}) => {
 
-  if (blog.user && user && blog.user.username !== user.username) {
-    return null
-  } else {
-    return (<button type="button" onClick={onClick} >delete</button>)
-  }
-}
-
-DeleteButton.propTypes = {
-  onClick: PropTypes.func.isRequired,
-  blog: PropTypes.shape({
-    user: PropTypes.shape({
-      username: PropTypes.string
-    })
-  }).isRequired,
-  user: PropTypes.shape({
-    username: PropTypes.string
-  }).isRequired
-}
-
-class BlogItem extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state =
-    {
-      blog: props.blog,
-      showFullInfo: false,
-      deleteFunction: props.deleteFunction,
-      currentUser: props.currentUser
-    }
-  }
-
-  toggleFullInfo = () => {
-    this.setState({
-      showFullInfo: !this.state.showFullInfo
-    })
-  }
-
-  addLike = async () => {
-    const blog = this.state.blog
-    blog.likes += 1
-    
-    try {
-      await blogService.update(blog)
-      this.setState({
-        blog: blog
-      })
-    } catch (exception) {
-        console.log(exception)
-    }
-  }
-
-  handleDelete = () => {
- 
-    this.state.deleteFunction(this.state.blog)
-  }
-
-  render() {
-    const blogStyle = {
-      paddingTop: 10,
-      paddingLeft: 2,
-      border: 'solid',
-      borderWidth: 1,
-      marginBottom: 5
-    }
-
-    const showWhenFullInfo= {
-      display: this.state.showFullInfo ? '' :'none',
-      paddingTop: 5,
-      paddingLeft: 15 
-    }
-
-        const userInfo= this.state.blog.user ? this.state.blog.user.name : ""
-        
-    return (
-      <div style={blogStyle} >
-        <div onClick={this.toggleFullInfo}>
-         {this.state.blog.title}: {this.state.blog.author}
-        </div>
-        <div style={showWhenFullInfo}>
-          <a href={this.state.blog.url} >{this.state.blog.url}</a><br/>
-          {this.state.blog.likes} likes <button type="button" onClick={this.addLike}>like</button><br/>
-          added by {userInfo}<br/>
-          <DeleteButton onClick={this.handleDelete} blog={this.state.blog} user={this.state.currentUser}/>
-
-        </div>  
-      </div>
-    )
-
-  }
-}
-
-BlogItem.propTypes = {
-  blog: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    author: PropTypes.string.isRequired,
-    url: PropTypes.string.isRequired,
-    likes: PropTypes.number.isRequired,
-    user: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      username: PropTypes.string.isRequired
-    })
-  }).isRequired,
-  deleteFunction:PropTypes.func.isRequired, 
-  currentUser: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    username: PropTypes.string.isRequired
-  }).isRequired
-}
 
 class App extends React.Component {
   constructor(props) {
@@ -410,7 +302,7 @@ class App extends React.Component {
         {this.state.blogs
           .sort((blog_a,blog_b) => {return blog_b.likes - blog_a.likes})
           .map(blog => 
-            <BlogItem key={blog._id} blog={blog} deleteFunction={this.deleteBlog} currentUser={this.state.user}/>
+            <Blog key={blog._id} blog={blog} deleteFunction={this.deleteBlog} currentUser={this.state.user}/>
         )}
       </div>
     );
--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,12 +1,116 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import blogService from '../services/blogs'
 
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+const DeleteButton = ({onClick,blog,user}) => {
 
+    if (blog.user && user && blog.user.username !== user.username) {
+      return null
+    } else {
+      return (<button type="button" onClick={onClick} >delete</button>)
+    }
+  }
+  
+  DeleteButton.propTypes = {
+    onClick: PropTypes.func.isRequired,
+    blog: PropTypes.shape({
+      user: PropTypes.shape({
+        username: PropTypes.string
+      })
+    }).isRequired,
+    user: PropTypes.shape({
+      username: PropTypes.string
+    }).isRequired
+  }
+  
+class Blog extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state =
+      {
+        blog: props.blog,
+        showFullInfo: false,
+        deleteFunction: props.deleteFunction,
+        currentUser: props.currentUser
+      }
+    }
+  
+    toggleFullInfo = () => {
+      this.setState({
+        showFullInfo: !this.state.showFullInfo
+      })
+    }
+  
+    addLike = async () => {
+      const blog = this.state.blog
+      blog.likes += 1
+      
+      try {
+        await blogService.update(blog)
+        this.setState({
+          blog: blog
+        })
+      } catch (exception) {
+          console.log(exception)
+      }
+    }
+  
+    handleDelete = () => {
+   
+      this.state.deleteFunction(this.state.blog)
+    }
+  
+    render() {
+      const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+      }
+  
+      const showWhenFullInfo= {
+        display: this.state.showFullInfo ? '' :'none',
+        paddingTop: 5,
+        paddingLeft: 15 
+      }
+  
+          const userInfo= this.state.blog.user ? this.state.blog.user.name : ""
+          
+      return (
+        <div style={blogStyle} >
+          <div onClick={this.toggleFullInfo}>
+           {this.state.blog.title}: {this.state.blog.author}
+          </div>
+          <div style={showWhenFullInfo}>
+            <a href={this.state.blog.url} >{this.state.blog.url}</a><br/>
+            {this.state.blog.likes} likes <button type="button" onClick={this.addLike}>like</button><br/>
+            added by {userInfo}<br/>
+            <DeleteButton onClick={this.handleDelete} blog={this.state.blog} user={this.state.currentUser}/>
+  
+          </div>  
+        </div>
+      )
+  
+    }
+  }
+  
+  Blog.propTypes = {
+    blog: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+      likes: PropTypes.number.isRequired,
+      user: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        username: PropTypes.string.isRequired
+      })
+    }).isRequired,
+    deleteFunction:PropTypes.func.isRequired, 
+    currentUser: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      username: PropTypes.string.isRequired
+    }).isRequired
+  }
 
-
-export default Blog
+  export default Blog
